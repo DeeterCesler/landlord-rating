66b391bd8fd827653396cbc4b86f086823bf9448
Deeter <deeter.cesler@gmail.com>
2018-10-19 15:33:13 -0600

    authentication views, landlord views, add new landlord

--- controllers/landlordController.js
+++ controllers/landlordController.js
@@ -2,9 +2,25 @@ const express = require("express");
 const router = express.Router();
 const LandLord = require("../models/landlords");
 
-router.get("/", (req, res) => {
-    res.render("landlords/new.ejs");
-})
+router.get("/", async (req, res) => {
+    try {
+        const foundLandlords = await LandLord.find({});
+        res.render("landlords/index.ejs", {
+            landlords: foundLandlords
+        });
+    }catch(err){
+        res.send(err);
+    };
+});
 
+router.get("/new", (req, res) => {
+    res.render("landlords/new.ejs");
+});
+
+router.post("/", async (req, res) => {
+    await LandLord.create(req.body);
+    console.log(req.body);
+    res.redirect("/landlords");
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
-// require("./db/db");
+require("./db/db");
 const userController = require("./controllers/userController");
 const authController = require("./controllers/authController");
 const landlordController = require("./controllers/landlordController");
@@ -17,7 +17,7 @@ app.use("/users", userController);
 app.use("/auth", authController);
 app.use("/landlords", landlordController);
 app.get("/", (req, res)=> {
-    res.send("THIS SHIT LIVE");
+    res.render("index.ejs");
 })
 
 const port = 3000;
