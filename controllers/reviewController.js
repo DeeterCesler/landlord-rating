ab2b6cc064e66b4949894c10a8ffe840227f3301
Kelly <kelly@Jerrys-MacBook-Pro.local>
2018-10-20 13:34:26 -0600

    review controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -7,17 +7,43 @@ const Users = require('../models/users')
 
 router.get('/', async (req, res) => {
     try {
-        const foundLandlords = await Landlord.find({});
+        const foundLandlords = await Landlords.find({});
         const foundReviews = await Reviews.find({});
-        const foundUsers = await 
+        const foundUsers = await Users.find({});
         res.render('reviews/index.ejs', {
            landords: foundLandlords,
-           reviews: foundReviews
+           reviews: foundReviews,
+           users: foundUsers,
         })
     } catch (err) {
         res.send(err)
     }
+  })
+
+router.get('/new', async (req,res) => {
+    const foundLandlords = await Landlords.find({});
+    const foundUser = await Users.findById({});
+    res.render('reviews/new.ejs', {
+        landords: foundLandlords,
+        users: foundUser,
+    })
 })
+
+router.get('/:id', async (req, res) => {
+    try {
+    const foundReview = await Reviews.findById(req.params.id);
+    const foundLandlord = await Landlords.findOne({'reviews._id': req.params.id});
+    const foundUser = await Users.findOne({'reviews._id': req.params.id});
+    res.render('reviews/show.ejs', {
+        reviews: foundReview,
+        landords: foundLandlord,
+        user: foundUser,
+        })
+    } catch err {
+
+    }
+})
+
 
 
 module.exports = router;
